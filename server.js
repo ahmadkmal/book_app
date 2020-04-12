3d68c83d8de30c77a83b24dc13180e889490ef0a
ahmadkmal <eng.ahmadkmal@gmail.com>
2020-04-12 23:41:27 +0300

    done depolymet init

---
+++ server.js
@@ -0,0 +1,13 @@
+'use strict';
+require('dotenv').config();
+const express = require('express');
+const superagent = require('superagent');
+const PORT = process.env.PORT || 4000;
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('pages/index');
+});
+app.listen(PORT, () => console.log(`I'm running at port ${PORT}`));
