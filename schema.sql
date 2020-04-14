f1b54c97fa8b19afa907a065862c9e944d34f849
ahmadkmal <eng.ahmadkmal@gmail.com>
2020-04-14 08:51:14 +0300

    done lab 12

---
+++ schema.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS books;
+CREATE TABLE books (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255),
+  author VARCHAR(255),
+  description TEXT,
+  imageURL TEXT,
+  due DATE NOT NULL DEFAULT NOW()
+)
